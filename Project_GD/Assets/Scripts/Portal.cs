bf236d5d13f35c34d913b2b2e4dbc7c6b19d9025
donghaDev <djfdmator@gmail.com>
2026-07-03 00:04:05 +0900

    멀까이건

--- Project_GD/Assets/Scripts/Portal.cs
+++ Project_GD/Assets/Scripts/Portal.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using Unity.VisualScripting;
 
 public class Portal : MonoBehaviour
 {
     [SerializeField] 
     private string sceneName;
-
+    [SerializeField] 
+    private AudioSource audioSource;    
     public void Go()
     {
+        StartCoroutine(GoRoutine());
+    }
+
+    private IEnumerator GoRoutine()
+    {
+        audioSource.Play();
+        yield return new WaitForSeconds(2.0f);
         SceneManager.LoadScene(sceneName);
     }
 }
